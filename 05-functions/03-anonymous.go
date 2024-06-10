861612b42131def7f6e6d00298bac53df1a21a13
Magesh Kuppan <tkmagesh77@gmail.com>
2024-06-10 16:43:14 +0530

    updates

--- 05-functions/03-anonymous.go
+++ 05-functions/03-anonymous.go
@@ -21,39 +21,34 @@ func main() {
 
 	//TODO: replace the following with anonymous functions
 
-	fmt.Println(add(100, 200))
+	/*
+		fmt.Println(func(x, y int) int {
+			return x + y
+		}(100, 200))
+	*/
+
+	result := func(x, y int) int {
+		return x + y
+	}(100, 200)
+	fmt.Println(result)
 
 	// fmt.Println(divide(100, 7))
 	// receiving the results in variables
-	q, r := divide(100, 7)
+	q, r := func(x, y int) (quotient, remainder int) {
+		quotient = x / y
+		remainder = x % y
+		return
+	}(100, 7)
+
 	fmt.Printf("Dividing 100 by 7, quotient = %d and remainder = %d\n", q, r)
 
 	// receiving partial results
-	/*
-		q, _ := divide(100, 7)
-		fmt.Printf("Dividing 100 by 7, quotient = %d\n", q)
-	*/
 
-}
+	q, _ = func(x, y int) (quotient, remainder int) {
+		quotient = x / y
+		remainder = x % y
+		return
+	}(100, 7)
+	fmt.Printf("Dividing 100 by 7, quotient = %d\n", q)
 
-// 2 parameters & 1 result
-func add(x, y int) int {
-	return x + y
-}
-
-// 2 parameters & 2 results
-/*
-func divide(x, y int) (int, int) {
-	quotient := x / y
-	remainder := x % y
-	return quotient, remainder
-}
-*/
-
-// 2 parameters & 2 results - using "named" results
-// "named" results are preferred when there are more than 1 return result (convention)
-func divide(x, y int) (quotient, remainder int) {
-	quotient = x / y
-	remainder = x % y
-	return
 }
