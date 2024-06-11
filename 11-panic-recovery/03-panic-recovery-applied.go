e56a715cd05ad6b7ac8d069523c82a6b454602c8
Magesh Kuppan <tkmagesh77@gmail.com>
2024-06-11 16:48:12 +0530

    panic & recovery applied

--- 11-panic-recovery/03-panic-recovery-applied.go
+++ 11-panic-recovery/03-panic-recovery-applied.go
@@ -12,9 +12,24 @@ func main() {
 	for {
 		fmt.Println("Enter the divisor")
 		fmt.Scanln(&divisor)
-		q, r := divide(100, divisor)
-		fmt.Printf("Dviding 100 by %d, quotient = %d and remainder = %d\n", divisor, q, r)
+		if q, r, err := divideClient(100, divisor); err == nil {
+			fmt.Printf("Dviding 100 by %d, quotient = %d and remainder = %d\n", divisor, q, r)
+			continue
+		} else {
+			fmt.Printf("Error occured: %v , please try again!\n", err)
+		}
 	}
+}
+
+func divideClient(x, y int) (quotient, remainder int, err error) {
+	defer func() {
+		if e := recover(); e != nil {
+			err = e.(error)
+			return
+		}
+	}()
+	quotient, remainder = divide(x, y)
+	return
 }
 
 // 3rd party api (CANNOT change this code)
