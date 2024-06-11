0fc0a7fb7d6ecbed11ae6ae5a8606a8d0482c2fb
Magesh Kuppan <tkmagesh77@gmail.com>
2024-06-11 15:30:58 +0530

    updates

---
+++ 09-errors/errors-applied.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+
+}
+
+func divide(x, y int) /* returning quotient and remainder */ {
+	/*
+		return an error if y == 0
+		otherwise return quotient & remainder
+	*/
+}
