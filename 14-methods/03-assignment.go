a7e665d137e44692eef98d1de80536f106e82cc2
Magesh Kuppan <tkmagesh77@gmail.com>
2024-06-12 11:43:37 +0530

    structs & methods - assignment

---
+++ 14-methods/03-assignment.go
@@ -0,0 +1,23 @@
+/*
+Create the following types
+	Circle
+		- Radius (float64)
+	Rectangle
+		- Length (float64)
+		- Width (float64)
+
+Create the following Methods for both the types
+	- Area() float64
+	- Perimeter() float64
+
+Formula
+	Circle Area = math.Pi * Radius * Radius
+	Circle Perimeter = 2 * math.Pi * Radius
+
+	Rectangle Area = Length * Width
+	Rectangle Perimeter = 2 * (Length + Width)
+
+In the main()
+	Create instances of Circle & Rectangle and Print the Area & Perimiter
+
+*/
