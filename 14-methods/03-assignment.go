8b1e4906cb0566b8c442374be250a543f59b47ca
Magesh Kuppan <tkmagesh77@gmail.com>
2024-06-12 11:56:21 +0530

    structs & methods - assignment - solution

--- 14-methods/03-assignment.go
+++ 14-methods/03-assignment.go
@@ -21,3 +21,45 @@ In the main()
 	Create instances of Circle & Rectangle and Print the Area & Perimiter
 
 */
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+type Rectangle struct {
+	Length float64
+	Width  float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Length * r.Width
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Length + r.Width)
+}
+
+func main() {
+	c := Circle{Radius: 12}
+	fmt.Println("Area : ", c.Area())
+	fmt.Println("Perimter : ", c.Perimeter())
+
+	r := Rectangle{Length: 10, Width: 12}
+	fmt.Println("Area : ", r.Area())
+	fmt.Println("Perimter : ", r.Perimeter())
+}
