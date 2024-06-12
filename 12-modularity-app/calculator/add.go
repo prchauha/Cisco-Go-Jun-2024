02bd08742b15c83564dc22719ef81d99babcff02
Magesh Kuppan <tkmagesh77@gmail.com>
2024-06-12 10:08:56 +0530

    updates

--- 12-modularity-app/calculator/add.go
+++ 12-modularity-app/calculator/add.go
@@ -1,5 +1,12 @@
 package calculator
 
+import "fmt"
+
+func init() {
+	fmt.Println("calculator initialized - [add.go]")
+}
+
 func Add(x, y int) int {
+	opCount["add"]++
 	return x + y
 }
--- 12-modularity-app/main.go
+++ 12-modularity-app/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 
+	"github.com/fatih/color"
 	"github.com/tkmagesh/cisco-go-jun-2024/12-modularity-app/calculator"
+	ut "github.com/tkmagesh/cisco-go-jun-2024/12-modularity-app/calculator/utils"
 )
 
 func main() {
+	color.Red("Welcome!")
 	fmt.Println(calculator.Add(100, 200))
 	fmt.Println(calculator.Subtract(100, 200))
+	fmt.Println(calculator.OpCount())
+	fmt.Println(ut.IsEven(21))
 	run()
 }
